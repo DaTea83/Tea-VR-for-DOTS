6bad8e1dbf0dbadbd0522b4585b61913df8bf442
DaTea83 <eugenechoy83@gmail.com>
2026-03-09 23:59:42 +0800

    ChoyUtilities is now a package

---
+++ Packages/com.datea83.vrfordots/Runtime/Core/Bootstrap.cs
@@ -0,0 +1,11 @@
+﻿using EugeneC.Singleton;
+using UnityEngine;
+
+namespace VRForDOTS.Core
+{
+    [DisallowMultipleComponent]
+    public sealed class Bootstrap : GenericSingleton<Bootstrap>
+    {
+        
+    }
+}
